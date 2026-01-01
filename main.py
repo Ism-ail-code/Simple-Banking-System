6ddc1ace58601005f6d6eadf7f499015e7c8ed00
Muhammad Ismail <aalikhanpubg@gmail.com>
2026-01-01 09:26:10 +0500

    added some final touchup

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def load_data():
         return []
 def load_trans_data():
     try:
-        with open("transation details.json","r") as file:
+        with open("transaction details.json", "r") as file:
             return json.load(file)
     except:
         return []
 def save_trans_data(tra):
-    with open("transation details.json","w") as file:
+    with open("transaction details.json", "w") as file:
         json.dump(transaction_data,file,indent=4)
 def save_data(data):
     try:
@@ -43,15 +43,17 @@ def check_balance():
         print("No Account Found")
         print("*" * 30)
 def withdraw():
-    acc_num=int(input("Enter Clients Account Number to withdraw money:  "))
+    acc_num=int(input("Enter Account Holder Account Number to withdraw money:  "))
     for account in data:
         if account["account_number"]==acc_num:
             withdraw_amount=int(input("Enter the amount to withdraw: "))
+            print(f"The Current Balance is {account["current_balance"]}")
             if 0 < withdraw_amount <= account["current_balance"]:
                 account["current_balance"]=account["current_balance"]-withdraw_amount
                 print(f"The amount of {withdraw_amount} has been credited from {account['name']} account")
                 print(f"The remaining balance is {account['current_balance']}")
                 save_data(data)
+                #This is for transaction details
                 transaction_data.append({
                     "account_number":account["account_number"],
                     "name":account["name"],
@@ -69,6 +71,7 @@ def deposit():
                 account["current_balance"]=account["current_balance"]+deposit_amount
                 print(f"Amount of {deposit_amount}  has been deposited to {account["account_number"]} name {account["name"]}")
                 print(f"The current balance is: {account["current_balance"]}")
+                #this is for transaction details
                 transaction_data.append({
                     "account_number": account["account_number"],
                     "name": account["name"],
@@ -83,6 +86,7 @@ def transaction_history():
         if account["account_number"] == acc_num:
             print(account)
 def acc_num_gen():
+    #This function is responsible for assigning random and unique account number to new User
     random_number=rd.randint(100000000,999999999)
     if random_number not in used_number:
         used_number.add(random_number)
@@ -117,3 +121,6 @@ def main():
                 return
 if __name__=="__main__":
     main()
+
+
+ 
